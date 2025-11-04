3b4b4f804d1f6c4f7ba6dedfc6fac9a6beae0785
minsoo <kevin1231001@gmail.com>
2025-11-04 22:37:56 +0900

    tiny malloc + adder.html + video

--- webproxy-lab/tiny/tiny.c
+++ webproxy-lab/tiny/tiny.c
@@ -142,7 +142,6 @@ void get_filetype(char *filename, char *filetype)
     else if (strstr(filename, ".jpg") || strstr(filename, ".jpeg")) // jpg/jpeg 통합
         strcpy(filetype, "image/jpeg");
         
-    /* --- 11.7 문제의 핵심 --- */
     else if (strstr(filename, ".mpg") || strstr(filename, ".mpeg"))
         strcpy(filetype, "video/mpeg");
     else if (strstr(filename, ".mp4"))
@@ -155,64 +154,119 @@ void get_filetype(char *filename, char *filetype)
 
 
 
-// serve_static 함수의 핵심 역할은 클라이언트가 요청한 정적 파일을 찾아서 HTTP 응답으로 보내주는 것.
-// int fd -> 클라이언트(브라우저)와 연결된 소켓(통신 파이프)의 번호
-// char *filename -> (파일 이름), 서버가 클라이언트에게 보내줘야할 파일의 로컬 경로이다.
-// int filesize -> (파일 크기), filename에 해당하는 파일의 정확한 크기(바이트 단위)이다.
-// doit 함수에서 읽을 수 있는지 확인한 후 서빙
+// // serve_static 함수의 핵심 역할은 클라이언트가 요청한 정적 파일을 찾아서 HTTP 응답으로 보내주는 것.
+// // int fd -> 클라이언트(브라우저)와 연결된 소켓(통신 파이프)의 번호
+// // char *filename -> (파일 이름), 서버가 클라이언트에게 보내줘야할 파일의 로컬 경로이다.
+// // int filesize -> (파일 크기), filename에 해당하는 파일의 정확한 크기(바이트 단위)이다.
+// // doit 함수에서 읽을 수 있는지 확인한 후 서빙
+// void serve_static(int fd, char *filename, int filesize)
+// {
+//   int srcfd; // 소스 파일 디스크립터
+//   char *srcp; // 소스 파일이 매핑될 메모리 주소 포인터
+//   char filetype[MAXLINE], buf[MAXBUF]; // 파일의 MIME 타입을 저장할 버퍼, 헤더 한 줄을 만들때 사용할 임시 버퍼
+
+//   // 응답 헤더 전송 : 지금부터 보낼 파일은 이런 종류이고, 크기는 이 정도야라고 알려주는 메타데이터를 전송한다.
+  
+//   // filename 기반으로 filetype 변수에 MIME 타입을 채운다.
+//   get_filetype(filename, filetype);
+
+//   // 헤더를 누적하지 않고 개별적으로 전송
+//   // buf 버퍼에 (성공) 상태 코드를 쓴.다
+//   sprintf(buf, "HTTP/1.0 200 OK\r\n");
+//   Rio_writen(fd, buf, strlen(buf));
+
+//   // 응답은 Tiny Web Server가 보낸 거야"라는 헤더를 만들어 전송한다.
+//   sprintf(buf, "Server: Tiny Web Server\r\n");
+//   Rio_writen(fd, buf, strlen(buf));
+
+//   // 이 응답이 끝나면 연결을 닫을 거야 라고 알려준다.
+//   sprintf(buf, "Connection: close\r\n"); // Tiny는 연결 유지를 지원 안 함
+//   Rio_writen(fd, buf, strlen(buf));
+
+//   // doit에서 받아온 filesize 값을 이용해 네가 받을 파일의 총 크기는 ()바이트야 라고 알려준다.
+//   sprintf(buf, "Content-length: %d\r\n", filesize); // 파일 크기
+//   Rio_writen(fd, buf, strlen(buf));
+
+//   sprintf(buf, "Content-type: %s\r\n\r\n", filetype); // MIME 타입과 헤더의 끝
+//   Rio_writen(fd, buf, strlen(buf));
+  
+//   // 서버 콘솔에 보낸 헤더 내용을 로깅(출력)한다.
+//   // (디버깅 목적으로 남겨두지만, 실제로는 위에서 buf의 내용을 실시간으로 출력해야 함)
+//   printf("Response headers sent (see individual lines above).\n");
+
+
+//   // 응답 본문 전송 : 파일의 실제 내용(알맹이) 전송
+
+//   // 전송할 파일을 읽기 전용(O_RDONLY)으로 연다. -> 수정을 하지 않기 때문에 읽기 전용으로 사용해도 충분하다.
+//   srcfd = Open(filename, O_RDONLY, 0);
+
+//   // 파일을 메모리에 매핑한다.
+//   srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+
+//   // 파일이 메모리에 매핑되었으므로 파일 디스크립터는 더 이상 필요없다. 즉시 닫아서 리소스 누수를 방지한다.
+//   Close(srcfd);
+
+//   // 파일을 클라이언트로 전송한다.
+//   Rio_writen(fd, srcp, filesize);
+
+//   // 매핑했던 메모리를 해제한다.
+//   Munmap(srcp, filesize);
+// }
+// In serve_static() 함수:
+
+/*
+ * serve_static - 정적 파일을 서빙합니다.
+ * [11.9 문제: Malloc/Rio_readn/Rio_writen 방식으로 수정됨]
+ */
+
 void serve_static(int fd, char *filename, int filesize)
 {
-  int srcfd; // 소스 파일 디스크립터
-  char *srcp; // 소스 파일이 매핑될 메모리 주소 포인터
-  char filetype[MAXLINE], buf[MAXBUF]; // 파일의 MIME 타입을 저장할 버퍼, 헤더 한 줄을 만들때 사용할 임시 버퍼
+    int srcfd; // 소스 파일 디스크립터
+    char *srcp; // [11.9] mmap 포인터 대신 malloc 포인터로 재활용
+    char filetype[MAXLINE], buf[MAXBUF]; // 헤더 전송용 버퍼
 
-  // 응답 헤더 전송 : 지금부터 보낼 파일은 이런 종류이고, 크기는 이 정도야라고 알려주는 메타데이터를 전송한다.
-  
-  // filename 기반으로 filetype 변수에 MIME 타입을 채운다.
-  get_filetype(filename, filetype);
+    /* 1. 응답 헤더 전송 */
+    get_filetype(filename, filetype);
+    sprintf(buf, "HTTP/1.1 200 OK\r\n");
+    sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
+    sprintf(buf, "%sConnection: close\r\n", buf);
+    sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
+    sprintf(buf, "%sContent-type: %s\r\n\r\n", buf, filetype);
+    
+    /* (참고: 헤더를 이렇게 누적해서 한 번에 보내는 것이 더 효율적입니다) */
+    Rio_writen(fd, buf, strlen(buf)); 
+    printf("Response headers sent.\n");
 
-  // 헤더를 누적하지 않고 개별적으로 전송
-  // buf 버퍼에 (성공) 상태 코드를 쓴.다
-  sprintf(buf, "HTTP/1.0 200 OK\r\n");
-  Rio_writen(fd, buf, strlen(buf));
+    /* 2. 응답 본문 전송 (11.9 수정된 부분) */
 
-  // 응답은 Tiny Web Server가 보낸 거야"라는 헤더를 만들어 전송한다.
-  sprintf(buf, "Server: Tiny Web Server\r\n");
-  Rio_writen(fd, buf, strlen(buf));
+    // /* [오류 수정] 'int srcfd'가 아니라, 이미 선언된 'srcfd'에 할당 */
+    // srcfd = Open(filename, O_RDONLY, 0);
 
-  // 이 응답이 끝나면 연결을 닫을 거야 라고 알려준다.
-  sprintf(buf, "Connection: close\r\n"); // Tiny는 연결 유지를 지원 안 함
-  Rio_writen(fd, buf, strlen(buf));
+    // /* [오류 수정] 헤더용 'buf' 대신 'srcp' 포인터 사용 */
+    // srcp = Malloc(filesize); // 'filesize'만큼 힙 메모리 할당
+    
+    // Rio_readn(srcfd, srcp, filesize); // 파일 내용을 힙(srcp)으로 읽기
+    // Close(srcfd); // 파일 닫기
+    
+    // Rio_writen(fd, srcp, filesize); // 힙(srcp)의 내용을 클라이언트로 전송
+    
+    // Free(srcp); // 할당했던 힙 메모리 해제
+      // 전송할 파일을 읽기 전용(O_RDONLY)으로 연다. -> 수정을 하지 않기 때문에 읽기 전용으로 사용해도 충분하다.
+    srcfd = Open(filename, O_RDONLY, 0);
 
-  // doit에서 받아온 filesize 값을 이용해 네가 받을 파일의 총 크기는 ()바이트야 라고 알려준다.
-  sprintf(buf, "Content-length: %d\r\n", filesize); // 파일 크기
-  Rio_writen(fd, buf, strlen(buf));
+    // 파일을 메모리에 매핑한다.
+    srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
 
-  sprintf(buf, "Content-type: %s\r\n\r\n", filetype); // MIME 타입과 헤더의 끝
-  Rio_writen(fd, buf, strlen(buf));
-  
-  // 서버 콘솔에 보낸 헤더 내용을 로깅(출력)한다.
-  // (디버깅 목적으로 남겨두지만, 실제로는 위에서 buf의 내용을 실시간으로 출력해야 함)
-  printf("Response headers sent (see individual lines above).\n");
+    // 파일이 메모리에 매핑되었으므로 파일 디스크립터는 더 이상 필요없다. 즉시 닫아서 리소스 누수를 방지한다.
+    Close(srcfd);
 
+    // 파일을 클라이언트로 전송한다.
+    Rio_writen(fd, srcp, filesize);
 
-  // 응답 본문 전송 : 파일의 실제 내용(알맹이) 전송
-
-  // 전송할 파일을 읽기 전용(O_RDONLY)으로 연다. -> 수정을 하지 않기 때문에 읽기 전용으로 사용해도 충분하다.
-  srcfd = Open(filename, O_RDONLY, 0);
-
-  // 파일을 메모리에 매핑한다.
-  srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
-
-  // 파일이 메모리에 매핑되었으므로 파일 디스크립터는 더 이상 필요없다. 즉시 닫아서 리소스 누수를 방지한다.
-  Close(srcfd);
-
-  // 파일을 클라이언트로 전송한다.
-  Rio_writen(fd, srcp, filesize);
-
-  // 매핑했던 메모리를 해제한다.
-  Munmap(srcp, filesize);
+    // 매핑했던 메모리를 해제한다.
+    Munmap(srcp, filesize);
 }
+
 
 void serve_dynamic(int fd, char *filename, char *cgiargs)
 {
@@ -251,7 +305,7 @@ void doit(int fd)
     sscanf(buf, "%s %s %s", method, uri, version);
 
     /* Only GET method is implemented */
-    if (strcasecmp(method, "GET")) {
+    if (strcasecmp(method, "GET")) { // head 메소드도 허용하도록 허용
         clienterror(fd, method, "501", "Not implemented",
                     "Tiny does not implement this method");
         return;
